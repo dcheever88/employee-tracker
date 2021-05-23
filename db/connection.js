eb9bdc6c1ef5a8dfe87ff97aad1f6463a5ac7848
dcheever <cheeverdanny@gmail.com>
2021-05-23 16:34:39 -0500

    add sql files

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 // connect to database
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "furyroad1",
-    database: ""
+    database: "employees"
 },
 console.log("connected to the employee database")
 );
