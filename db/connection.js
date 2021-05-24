6b18aa488c1b4c4035607b0f262423d1dee676e7
dcheever <cheeverdanny@gmail.com>
2021-05-23 19:19:43 -0500

    added routes

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,31 @@ const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
+// not sure about this
+// class Database {
+//     constructor(config) {
+//         this.connection = mysql.createConnection(config);
+//     }
+//     query(sql, args =[] ) {
+//         return new Promise((resolve, reject) => {
+//             this.connection.query(sql, args, (err, rows) => {
+//                 if (err)
+//                     return reject(err);
+//                 resolve(rows);
+//             });
+//         });
+//     }
+//     close() {
+//         return new Promise((resolve, reject) => {
+//             this.connection.end(err => {
+//                 if (err)
+//                     return reject(err);
+//                 resolve();
+//             });
+//         });
+//     }
+// }
+
 // connect to database
 const db = mysql.createConnection({
     host: "localhost",
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const cTable = require("console.table");
 const db = require("./db/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// not sure about this
+// const routes = require("./routes/routes");
 
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+// use routes (note sure about this)
+app.use("/api", routes);
 
 // default response for any other request (not found)
 app.use((req, res) => {
